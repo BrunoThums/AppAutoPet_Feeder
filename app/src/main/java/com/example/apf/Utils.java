6586a350cdf0cee6bba74566e0bd5f217822bcc2
Bruno Brill Thums <bbt-123@hotmail.com>
2021-11-04 20:48:37 -0300

    Verifica data

--- app/src/main/java/com/example/apf/Utils.java
+++ app/src/main/java/com/example/apf/Utils.java
@@ -5,6 +5,9 @@ import android.widget.EditText;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Utils {
     public static void formataCelular(EditText celular){
         SimpleMaskFormatter simpleMaskFormatter = new SimpleMaskFormatter("(NN)NNNNN-NNNN");
@@ -16,5 +19,26 @@ public class Utils {
         SimpleMaskFormatter simpleMaskFormatter = new SimpleMaskFormatter("NN/NN/NNNN");
         MaskTextWatcher maskTextWatcher = new MaskTextWatcher(data, simpleMaskFormatter);
         data.addTextChangedListener(maskTextWatcher);
+    }
+
+    public static boolean verificaData(EditText data){
+            try {
+
+                //SimpleDateFormat é usada para trabalhar com formatação de datas
+                //formato "dd/MM/yyyy"
+                //dd = dia, MM = mes, yyyy = ano
+                //"M" maiusculo = mês
+                //"m" minusculo = minutos
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                //setLenient() = "false" = não aceitar datas falsas como 31/02/2021
+                sdf.setLenient(false);
+                //converter String em um objeto do tipo date se funcionar
+                //se true = a data é valida
+                sdf.parse(data.getText().toString());
+                return true;
+            } catch (ParseException ex) {
+                //se der errado = a data é falsa
+                return false;
+            }
     }
 }
